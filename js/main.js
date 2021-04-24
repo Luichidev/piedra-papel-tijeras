0bef31fd7992870f45e9512057cdf44c04dcfbfc
@LuisArana <disconluis@gmail.com>
2021-04-24 17:23:18 +0200

    Se implemento el script de la logica del juego.

--- js/main.js
+++ js/main.js
@@ -2,34 +2,79 @@
 var empates = 0,
   victorias = 0,
   perdidas = 0,
-  partidas = 0;
+  partidas = 10;
 
 // funcion tirada del usuario
-
 function tirada(tiradaJugador) {
   // Determinar la tirada del CPU
   var tiradaCPU = Math.floor(Math.random() * 3);
   var tiradas = ["piedra", "papel", "tijera"];
   tiradaCPU = tiradas[tiradaCPU];
 
-  // actualizar GUI
-  imgJugador.src = "imagenes/" + tiradaJugador + ".png";
-  imgCPU.src = "imagenes/" + tiradaCPU + ".png";
+  if (partidas > 0) {
+    // actualizar GUI
+    imgJugador.src = "imagenes/" + tiradaJugador + ".png";
+    imgCPU.src = "imagenes/" + tiradaCPU + ".png";
 
-  //¿hay empate?
-  // si
+    //¿hay empate?
+    // si
+    if (tiradaJugador === tiradaCPU) {
+      // gana='nadie', empates++
+      outGanador.innerHTML = "Empate";
+      empates++;
+      //sino
+    } else {
+      // ¿Gana jugador?
+      // si
+      if (
+        (tiradaJugador === "piedra" && tiradaCPU === "tijera") ||
+        (tiradaJugador === "papel" && tiradaCPU === "piedra") ||
+        (tiradaJugador === "tijera" && tiradaCPU === "papel")
+      ) {
+        //gana='jugador', victorias++
+        outGanador.innerHTML = "Has ganado!";
+        victorias++;
+        //sino
+      } else {
+        // gana='CPU', derrotas++
+        outGanador.innerHTML = "Has perdido!";
+        perdidas++;
+      }
+    }
 
-  // gana='nadie', empates++
-  //sino
-  // ¿Gana jugador?
-  // si
-  //gana='jugador', victorias++
-  //sino
-  // gana='CPU', derrotas++
-  //partidas++
-  //actuliazar la interfaz gráfica
-  outEmpates.innerHTML = partidas;
-  outVictoria.innerHTML = partidas;
-  outPerdidas.innerHTML = partidas;
+    //partidas--
+    partidas--;
+
+    //actuliazar la interfaz gráfica
+    actualizarScore();
+  } else {
+    ganadorFinal.style.display = "block";
+    if (victorias === perdidas) {
+      outGanadorFinal.innerHTML = "Ha terminado en Empate!!!";
+    } else {
+      victorias > perdidas
+        ? (outGanadorFinal.innerHTML = "Ganaste!!!")
+        : (outGanadorFinal.innerHTML = "Gana la CPU!!!");
+    }
+
+    ganadorFinal.onclick = function () {
+      empates = 0;
+      victorias = 0;
+      perdidas = 0;
+      partidas = 10;
+      ganadorFinal.style.display = "none";
+      //actuliazar la interfaz gráfica
+      actualizarScore();
+      outGanador.innerHTML = "Empieza la partida";
+      imgJugador.src = "imagenes/back.png";
+      imgCPU.src = "imagenes/back.png";
+    };
+  }
+}
+
+function actualizarScore() {
+  outEmpates.innerHTML = empates;
+  outVictoria.innerHTML = victorias;
+  outPerdidas.innerHTML = perdidas;
   outPartidas.innerHTML = partidas;
 }
